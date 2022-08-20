2f3082c5e931acbccde1e03c1ccbb2b25b73b643
MarcoAurelio-26 <71940808+Marco-Aurelio-256@users.noreply.github.com>
2022-08-20 18:31:45 -0300

    +Video #14 - Css; +Video #15 - Props

---
+++ test1/src/components/BaseAlert.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="baseClass">
+    {{ test }}
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      default: '',
+    },
+    test: {
+      type: String,
+      default: 'Testando - ',
+    },
+
+  },
+
+  computed: {
+    baseClass(){
+      return [
+        'alert',
+        this.variant ? `alert-${this.variant}` : ''
+      ]
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.alert{
+  padding: 10px;
+  color: gray;
+  background: #ddd
+}
+
+.alert-sucess{
+  background: green;
+  color: white;
+}
+
+.alert-danger{
+  background: red;
+  color: white;
+}
+
+
+</style>
